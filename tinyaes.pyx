eedbc0836e8ec650ff66687b45a8e69e250698cc
Matteo Bertini <matteo@naufraghi.net>
2020-01-28 23:35:39 +0100

    Fix broken `uint8_t*` interface that truncates at the first NULL byte
    The generated Cython code truncates the key at the first NULL, parsing
    it as a null terminated string.
    
    We still offer a similar interface, the idiomatic one that operates on
    a copy of the input data, calling `bytes(data)`, and a fast one that
    operates on `bytearray`s only, mutating the data inplace.

--- tinyaes.pyx
+++ tinyaes.pyx
@@ -8,20 +8,21 @@ cdef uint8_t AES_BLOCKLEN = 16
 
 cdef class AES:
     cdef tinyaes.AES_ctx _ctx
-    def __cinit__(self, uint8_t* key, uint8_t* iv=NULL):
+    def __cinit__(self, bytes key, bytes iv=None):
+        # `uint8_t* data` interface is not usable, because the Cython wrapper
+        # detects the buffer lenght searching the final NULL character, that
+        # may occur inside the buffer itself.
         if len(key) != AES_KEYLEN:
             raise ValueError(f"AES128 needs a 16 bytes key, but len(key) = {len(key)}")
-        if iv is not NULL and len(iv) != AES_BLOCKLEN:
+        if iv is not None and len(iv) != AES_BLOCKLEN:
             raise ValueError(f"AES128 needs a 16 bytes iv (or nothing), but len(iv) = {len(iv)}")
-        if iv is NULL:
+        if iv is None:
             tinyaes.AES_init_ctx(&self._ctx, key)
         else:
             tinyaes.AES_init_ctx_iv(&self._ctx, key, iv)
-    cdef _CTR_xcrypt_buffer_raw(self, uint8_t* data):
+    def CTR_xcrypt_buffer(self, data):
+        inout = bytes(data)
         tinyaes.AES_CTR_xcrypt_buffer(&self._ctx, data, len(data))
-    def CTR_xcrypt_buffer(self, uint8_t* data):
-        inout = data[:]
-        self._CTR_xcrypt_buffer_raw(inout)
         return inout
     def CTR_xcrypt_buffer_inplace(self, bytearray data):
-        self._CTR_xcrypt_buffer_raw(data)
+        tinyaes.AES_CTR_xcrypt_buffer(&self._ctx, data, len(data))
